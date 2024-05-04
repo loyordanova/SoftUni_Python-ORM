1759b64c8dd8a6d7663054831c3deb3eb72e959a
loyordanova <lora.yordanova@yahoo.com>
2024-05-04 17:40:24 +0300

    Added more solutions

--- Django_models_exercise/orm_skeleton/settings.py
+++ Django_models_exercise/orm_skeleton/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'models_db',
+'USER': 'postgres-user',
+'PASSWORD': 'password',
+'HOST': '127.0.0.1',
+'PORT': '5432'
+}
 }
 
 
