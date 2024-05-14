9654937d879056170f8c7b9fb4d000284ba3cdea
loyordanova <lora.yordanova@yahoo.com>
2024-05-14 23:47:34 +0300

    Added more solutions

--- Working_with_queries_in_django_exercise/orm_skeleton/settings.py
+++ Working_with_queries_in_django_exercise/orm_skeleton/settings.py
@@ -82,17 +82,16 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "",
-#         "USER": "",
-#         "PASSWORD": "",
-#         "HOST": "localhost",
-#         "PORT": "5432",
-#     }
-# }
-
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydatabase4",
+        "USER": "postrges-user",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
