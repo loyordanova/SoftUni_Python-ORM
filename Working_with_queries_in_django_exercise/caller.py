be441661eb92d24b8e37615ecc2f79259d99994d
loyordanova <lora.yordanova@yahoo.com>
2024-05-16 15:16:16 +0300

    Added more solutions

--- Working_with_queries_in_django_exercise/caller.py
+++ Working_with_queries_in_django_exercise/caller.py
@@ -6,7 +6,7 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import ArtworkGallery, Laptop, ChessPlayer, Meal, Dungeon
+from main_app.models import ArtworkGallery, Laptop, ChessPlayer, Meal, Dungeon, Workout
 from django.db.models import Q, Case, When, Value
 
 # 01. Artwork Gallery -------------------------------------------------------
@@ -187,3 +187,43 @@ def set_new_locations():
             When(recommended_level=75, then=Value('Shadowed Abyss')),
         )
     )
+
+# 06. Workout --------------------------------------------------------------
+
+def show_workouts():
+    workouts = Workout.objects.filter(workout_type__in=['Calisthenics', 'CrossFit']).order_by('id')
+
+    workout_info = [
+        f'{workout.name} from {workout.workout_type} type has {workout.difficulty} difficulty!'
+        for workout in workouts
+    ]
+
+    return '\n'.join(workout_info)
+
+def get_high_difficulty_cardio_workouts():
+    return Workout.objects.filter(workout_type='Cardio', difficulty='High').order_by('instructor')
+
+def set_new_instructors():
+    Workout.objects.update(
+        instructor=Case(
+            When(workout_type='Cardio', then=Value('John Smith')),
+            When(workout_type='Strength', then=Value('Michael Williams')),
+            When(workout_type='Yoga', then=Value('Emily Johnson')),
+            When(workout_type='CrossFit', then=Value('Sarah Davis')),
+            When(workout_type='Calisthenics', then=Value('Chris Heria'))
+        )
+    )
+
+def set_new_duration_times():
+    Workout.objects.update(
+        duration=Case(
+            When(instructor='John Smith', then=Value('15 minutes')),
+            When(instructor='Sarah Davis', then=Value('30 minutes')),
+            When(instructor='Chris Heria', then=Value('45 minutes')),
+            When(instructor='Michael Williams', then=Value('1 hour')),
+            When(instructor='Emily Johnson', then=Value('1 hour and 30 minutes')),
+        )
+    )
+
+def delete_workouts():
+    Workout.objects.exclude(workout_type__in=['Strength', 'Calisthenics']).delete()
