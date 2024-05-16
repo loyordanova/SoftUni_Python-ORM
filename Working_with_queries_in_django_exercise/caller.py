b9e9a3a8f6327ca48fb278c4bd6ce0ea7a3a4e80
loyordanova <lora.yordanova@yahoo.com>
2024-05-16 11:17:27 +0300

    Added more solutions

--- Working_with_queries_in_django_exercise/caller.py
+++ Working_with_queries_in_django_exercise/caller.py
@@ -1,11 +1,13 @@
 import os
+from typing import List
 import django
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import ArtworkGallery
+from main_app.models import ArtworkGallery, Laptop
+from django.db.models import Q, Case, When, Value, F
 
 # 01. Artwork Gallery -------------------------------------------------------
 
@@ -30,3 +32,35 @@ artwork2 = ArtworkGallery(artist_name='Leonardo da Vinci', art_name='Mona Lisa',
 
 
 # 02. Laptop  -------------------------------------------------------
+
+def show_the_most_expensive_laptop():
+    top_laptop = Laptop.objects.order_by("-price", "id").first()
+
+    return f"{top_laptop.brand} is the most expensive laptop available for {top_laptop.price}$!"
+
+def bulk_create_laptops(*args):
+    Laptop.objects.bulk_create(*args)
+
+def update_to_512_GB_storage():
+    Laptop.objects.filter(brand__in=["Asus", "Lenovo"]).update(storage=512)
+
+    # another solution
+    # Laptop.objects.filter(Q(brand="Asus") | Q(brand="Lenovo").update(storage=512)
+
+def update_to_16_GB_memory():
+    Laptop.objects.filter(brand__in=["Apple", "Dell", "Acer"]).update(memory=16)
+
+def update_operation_systems():
+    Laptop.objects.update(
+        operation_system=Case(
+            When(brand="Asus", then=Value("Windows")),
+            When(brand="Apple", then=Value("MacOS")),
+            When(brand__in=["Dell", "Acer"], then=Value("Linux")),
+            When(brand="Lenovo", then=Value("Chrome OS"))
+        )
+    )
+    # another solution
+    
+
+def delete_inexpensive_laptops():
+    Laptop.objects.filter(price__lt=1200).delete()
--- Working_with_queries_in_django_exercise/main_app/models.py
+++ Working_with_queries_in_django_exercise/main_app/models.py
@@ -80,48 +80,25 @@ class ArtworkGallery(models.Model):
     )
 
 # 02. Laptop ---------------------------------------------------
-    
-    class Lapttop(models.Model):
-        BRAND_CHOICES = (
-            ("Asus", "Asus"),
-            ("Acer", "Acer"),
-            ("Apple", "Apple"),
-            ("Lenovo", "Lenovo"),
-            ("Dell", "Dell")
-        )
 
-        OS_CHOICES = (
-            ("Windows", "Windows"),
-            ("MacOS", "MacOS"),
-            ("Linux", "Linux"),
-            ("Chrome OS", "Chrome OS")
-        )
+class Laptop(models.Model):
+    LAPTOP_BRANDS = (
+        ("Asus", "Asus"),
+        ("Acer", "Acer"),
+        ("Apple", "Apple"),
+        ("Lenovo", "Lenovo"),
+        ("Dell", "Dell")
+    )
+    OPERATING_SYSTEMS = (
+        ("Windows", "Windows"),
+        ("MacOS", "MacOS"),
+        ("Linux", "Linux"),
+        ("Chrome OS", "Chrome OS")
+    )
 
-        brand = models.CharField(
-            max_length=20,
-            choices=BRAND_CHOICES
-        )
-
-        processor = models.CharField(
-            max_length=100
-        )
-
-        memory = models.PositiveIntegerField(
-            help_text="Memory in GB"
-        )
-
-        storage = models.PositiveIntegerField(
-            help_text="Storage in GB"
-        )
-
-        operation_system = models.CharField(
-            max_length=20,
-            choices=OS_CHOICES
-        )
-
-        price = models.DecimalField(
-            max_digits=10,
-            decilam_places=2
-        )
-
-# 03. Chess Player ---------------------------------------------------
+    brand = models.CharField(max_length=10, choices=LAPTOP_BRANDS)
+    processor = models.CharField(max_length=100)
+    memory = models.PositiveIntegerField(help_text="Memory in GB")
+    storage = models.PositiveIntegerField(help_text="Storage in GB")
+    operation_system = models.CharField(max_length=20, choices=OPERATING_SYSTEMS)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
