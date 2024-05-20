c8ef48eb93229a0eaae705f61d200f23bdc11274
loyordanova <lora.yordanova@yahoo.com>
2024-05-20 12:16:04 +0300

    Added more solutions

--- Django Models Relations - Exercise/caller.py
+++ Django Models Relations - Exercise/caller.py
@@ -5,8 +5,10 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import Author, Book
+from main_app.models import Author, Book, Artist, Song, Product, Review
 
+
+# 01. Library -----------------------------------------------------------
 
 def show_all_authors_with_their_books():
     authors = Author.objects.all().order_by("id")
@@ -19,5 +21,64 @@ def show_all_authors_with_their_books():
 
     return "\n".join(result)
 
+
 def delete_all_authors_without_books():
     Author.objects.filter(book__isnull=True).delete()
+
+
+# 02. Music App -----------------------------------------------------------
+    
+def add_song_to_artist(artist_name: str, song_title: str):
+    artist = Artist.objects.get(name=artist_name)
+    song = Song.objects.get(title=song_title)
+
+    artist.songs.add(song)
+
+
+def get_songs_by_artist(artist_name: str):
+    artist = Artist.objects.get(name=artist_name)
+    return artist.songs.all().order_by("-id")
+
+
+def remove_song_from_artist(artist_name: str, song_title: str):
+    artist = Artist.objects.get(name=artist_name)
+    song = Song.objects.get(title=song_title)
+
+    artist.songs.remove(song)
+
+
+# 03. Shop -----------------------------------------------------------
+    
+def calculate_average_rating_for_product_by_name(product_name: str):
+    product = Product.objects.get(name=product_name)
+    reviews = product.reviews.all()
+
+    total_rating = sum(r.rating for r in reviews)
+    avg_rating = total_rating / len(reviews)
+
+    return avg_rating
+
+    # better solution
+
+    # product = Product.objects.annotate(
+    #     total_ratings=Sum('review__rating'),
+    #     num_reviews = Count('review')
+    # ).get(name=product_name)
+
+    # avg_ratings = product.total_ratings / product.num_reviews
+
+    # return avg_ratings
+
+
+def get_reviews_with_high_ratings(threshold: int):
+    return Review.objects.filter(rating__gte=threshold)
+
+
+def get_products_with_no_reviews():
+    return Product.objects.filter(reviews__isnull=True).order_by('-name')
+
+
+def delete_products_without_reviews():
+    Product.objects.filter(reviews__isnull=True).delete()
+
+# 04. Licence -----------------------------------------------------------
--- Django Models Relations - Exercise/main_app/models.py
+++ Django Models Relations - Exercise/main_app/models.py
@@ -21,4 +21,70 @@ class Book(models.Model):
         on_delete=models.CASCADE
     )
 
+# 02.Music App -----------------------------------------------------------
+
+class Song(models.Model):
+    title = models.CharField(
+        max_length=100,
+        unique=True
+    )
+
+class Artist(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True
+    )
+
+    songs = models.ManyToManyField(
+        to=Song,
+        related_name='artists'
+    )
+
+
+# 02.Music App -----------------------------------------------------------
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True
+    )
+
+
+class Review(models.Model):
+    description = models.TextField(
+        max_length=200
+    )
+
+    rating = models.PositiveIntegerField()
+
+    product = models.ForeignKey(
+        to=Product,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+
+
+# 04. Licence -----------------------------------------------------------
     
+class Driver(models.Model):
+    first_name = models.CharField(
+        max_length=50
+    )
+
+    last_name = models.CharField(
+        max_length=50
+    )
+
+class DrivingLicense(models.Model):
+    license_number = models.CharField(
+        max_digits=10,
+        unique=True
+    )
+
+    issue_date = models.DateField()
+
+    driver = models.OneToOneField(
+        to=Driver,
+        on_delete=models.CASCADE,
+        related_name='license'
+    )
