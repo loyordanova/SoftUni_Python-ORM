a7e8c37338d26c0b6a8245bc9b74bd2f8255c0a2
loyordanova <lora.yordanova@yahoo.com>
2024-06-04 11:26:19 +0300

    Added more solutions

--- Models_inheritance_and_customization_lab/main_app/models.py
+++ Models_inheritance_and_customization_lab/main_app/models.py
@@ -1,8 +1,22 @@
+from datetime import date
 from django.db import models
+from django.forms import ValidationError
 
 # Create your models here.
 
+class BoolenaChoiceField(models.BooleanField):
+    def __init__(self, *args, **kwargs):
+        kwargs['choices'] = (
+            (True, 'Available'),
+            (False, 'Not Available')
+        )
+
+        kwargs['default'] = True
+
+        super().__init__(*args, **kwargs)
+
 # 01. Zoo Animals ---------------------------------------------------------
+# 06. Animal's Age --------------------------------------------------------
 
 class Animal(models.Model):
     name = models.CharField(
@@ -19,6 +33,15 @@ class Animal(models.Model):
         max_length=100
     )
 
+    def __str__(self):
+        return self.name
+    
+    @property
+    def age(self):
+        current_date = date.today()
+        return current_date.year - self.birth_date.year - ((current_date.month, current_date.day) < (self.birth_date.month, self.birth_date.day))
+
+    
 
 class Mammal(Animal):
     fur_color = models.CharField(
@@ -40,6 +63,7 @@ class Reptile(Animal):
 
 
 # 02. Zoo Employees --------------------------------------------------------
+# 04. Zookeeper's Specialty ------------------------------------------------
 
 class Employee(models.Model):
     first_name = models.CharField(
@@ -57,22 +81,31 @@ class Employee(models.Model):
     class Meta:
         abstract = True
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class ZooKeeper(Employee):
-    SPECIALTY_CHOICES = (
-        ('Mammals', 'Mammals'),
-        ('Birds', 'Birds'),
-        ('Reptiles', 'Reptiles'),
-        ('Others', 'Others')
-    )
+    class Specialties(models.TextChoices):
+        Mammals = 'Mammals'
+        Birds = 'Birds'
+        Reptiles = 'Reptiles'
+        Others = 'Others'
+             
     specialty = models.CharField(
         max_length=10,
-        choices=SPECIALTY_CHOICES
+        choices=Specialties.choices
     )
 
     managed_animals = models.ManyToManyField(
         to=Animal
     )
+
+    def clean(self):
+        super().clean()
+
+        if self.specialty not in self.Specialties:
+            raise ValidationError('Specialty must be a valid choice.')
 
 
 class Veterinarian(Employee):
@@ -80,8 +113,34 @@ class Veterinarian(Employee):
         max_length=10
     )
 
+    availability = BoolenaChoiceField()
+
+    def is_available(self):
+        return self.availability
+    
 
 # 03. Animal Display System ----------------------------------------------------
+# 05. Animal Display System Logic ----------------------------------------------
 
 class ZooDisplayAnimal(Animal):
+
+    class Meta:
+        proxy = True
+
+
+    def display_info(self):
+         return (
+            f"Meet {self.name}! Species: {self.species}, born {self.birth_date}. "
+            f"It makes a noise like '{self.sound}'."
+        )
+
     
+    def is_endangered(self):
+        endangered_species = [
+            'Cross River Gorilla', 'Orangutan', 'Green Turtle'
+        ]
+         
+        if self.species in endangered_species:
+            return f"{self.species} is at risk!"
+        else:
+            return f"{self.species} is not at risk."
