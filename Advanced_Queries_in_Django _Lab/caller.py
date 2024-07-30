25e025b8f4be4a86e0a21dfee19bf9ca3bb98f3a
loyordanova <lora.yordanova@yahoo.com>
2024-07-30 21:35:44 +0300

    Added more solutions

--- Advanced_Queries_in_Django _Lab/caller.py
+++ Advanced_Queries_in_Django _Lab/caller.py
@@ -6,8 +6,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models
-from main_app.models import Product, Category, Customer, Order, OrderProduct, product_quantity_ordered
-
+from main_app.models import Product, Category, Customer, Order, OrderProduct
+from django.db.models import Sum, Q, F
 
 # Create and run queries
 def add_records_to_database():
@@ -58,8 +58,37 @@ def add_records_to_database():
     return "All data entered!"
 
 
-# Run and print your queries
-# print(add_records_to_database())
-print(product_quantity_ordered())
+def product_quantity_ordered():
+    total_products_ordered = Product.objects.annotate(total_ordered_quantity=Sum('orderproduct__quantity')).exclude(total_ordered_quantity=None).order_by('-total_ordered_quantity')
 
+    result = []
+    for product in total_products_ordered:
+        result.append(f'Quantity ordered of {product.name}: {product.total_ordered_quantity}')
+    
+    return '\n'.join(result)
+
+def ordered_products_per_customer():
+    orders = Order.objects.prefetch_related('orderproduct_set__product__category').all().order_by('id')
+
+    result = []
+    for order in orders:
+        result.append(f"Order ID: {order.id}, Customer: {order.customer.username}")
+        for ordered_product in order.orderproduct_set.all():
+            result.append(f"- Product: {ordered_product.product.name}, Category: {ordered_product.product.category.name}")
+    
+    return '\n'.join(result)
+
+def filter_products():
+    query = Q(is_available=True) & Q(price__gt=3)
+    products = Product.objects.filter(query).order_by('-price', 'name')
+
+    result = []
+    for product in products:
+        result.append(f"{product.name}: {product.price}lv.")
+
+    return '\n'.join(result)
+
+# print(product_quantity_ordered())
+# print(ordered_products_per_customer())
+ 
 
